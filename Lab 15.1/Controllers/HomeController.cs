280110367de3965753b11ed347ad48247b107d48
Manny <mannysalazar@quickenloans.com>
2020-09-22 09:05:03 -0400

    Lab 15.1

---
+++ Lab 15.1/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Lab_15._1.Models;
+using System.Net.Http;
+
+namespace Lab_15._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Cards(string drawAgain, string deck_id)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://deckofcardsapi.com");
+            var response = await client.GetAsync("/api/deck/new/shuffle");
+            Hand hand;
+            if (string.IsNullOrEmpty(drawAgain))
+            {
+                Deck deck = await response.Content.ReadAsAsync<Deck>();
+                response = await client.GetAsync($"/api/deck/{deck.Deck_ID}/draw/?count=5");
+                hand = await response.Content.ReadAsAsync<Hand>();
+            }
+            else
+            {
+                response = await client.GetAsync($"/api/deck/{deck_id}/draw/?count=5");
+                hand = await response.Content.ReadAsAsync<Hand>();
+            }
+            return View(hand);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab 15.1/Models/Hand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_15._1.Models
+{
+    public class Hand
+    {
+        public bool Success { get; set; }
+        public CardDetails[] Cards { get; set; }
+        public string Deck_ID { get; set; }
+        public int Remaining { get; set; }
+    }
+}
